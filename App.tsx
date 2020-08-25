6113b5576f61b338ebed13e51c3338f6da729d41
Kelvin Liang <kelvin9877@gmail.com>
2020-08-26 00:28:51 +0800

    start to work on welcome screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+
+const slides = [
+  {
+    key: "one",
+    title: "Run For Health",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./src/images/Jogging-rafiki.png"),
+  },
+  {
+    key: "two",
+    title: "Run with Friends",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./src/images/Jogging-amico.png"),
+  },
+  {
+    key: "three",
+    title: "Run for relax",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./src/images/Jogging-pana.png"),
+  },
+];
 
 export default function App() {
   return (
