fd4fab2c1f4a0bcab7841eeecf2b221e9322edcb
Kelvin Liang <kelvin9877@gmail.com>
2020-08-26 15:42:46 +0800

    add custom fonts and app loading

--- App.tsx
+++ App.tsx
@@ -1,26 +1,45 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from "expo";
+import {
+  RobotoSlab_200ExtraLight,
+  RobotoSlab_400Regular,
+  RobotoSlab_500Medium,
+  RobotoSlab_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto-slab";
+
 import Welcome from "./src/screens/Welcome";
 
 const App: React.FC = (): JSX.Element => {
   const [showHomeScreen, setShowHomeScreen] = useState(false);
+  let [fontsLoaded] = useFonts({
+    RobotoSlab_200ExtraLight,
+    RobotoSlab_400Regular,
+    RobotoSlab_500Medium,
+    RobotoSlab_700Bold,
+  });
 
   const handleDone = () => {
     setShowHomeScreen(!showHomeScreen);
   };
 
-  return (
-    <View style={styles.container}>
-      {showHomeScreen ? (
-        <Text style={styles.text}>Run For Health</Text>
-      ) : (
-        <Welcome handleDone={handleDone} />
-      )}
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        {showHomeScreen ? (
+          <Text style={styles.text}>Run For Health</Text>
+        ) : (
+          <Welcome handleDone={handleDone} />
+        )}
 
-      <StatusBar style="auto" />
-    </View>
-  );
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -30,6 +49,7 @@ const styles = StyleSheet.create({
   },
   text: {
     marginTop: 200,
+    fontFamily: "RobotoSlab_700Bold",
     fontSize: 25,
     fontWeight: "bold",
     textAlign: "center",
